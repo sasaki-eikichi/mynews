2405ecb94af701a99b46d1a2cb81f9cd3d4efcab
佐々木　永吉 <>
2021-01-22 15:23:41 +0900

    “4-10”

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 
     Route::group(['prefix' => 'admin'], function() {
+        Route::get('news/create', 'Admin\NewsController@add');
         Route::get('profile/create', 'Admin\ProfileController@add');
         Route::get('profile/edit', 'Admin\ProfileController@edit');
       });
