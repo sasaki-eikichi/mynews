4e3a9a2a6542481ac7b8aa9fe3fb387b4981d037
佐々木　永吉 <>
2021-01-26 23:17:50 +0900

    “4-17投稿したニュース一覧を表示しよう”

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -21,7 +21,9 @@ public function create(Request $request)
     $this->validate($request, Profile::$rules);
     $profile = new Profile;
     $form = $request->all();
-
+    
+    unset($form['_token']);
+    unset($form['image']);
       // データベースに保存
       $profile->fill($form);
       $profile->save(); 
@@ -32,13 +34,26 @@ public function create(Request $request)
 
 public function edit()
 {
-    return view('admin.profile.edit');
+    return view('admin.profile.index');
 }
 
 public function update()
 {
-    return redirect('admin/profile/edit');
+    return redirect('admin/profile/index');
 }
+public function index(Request $request)
+{
+    $cond_title = $request->cond_title;
+    if ($cond_title != '') {
+        // 検索されたら検索結果を取得する
+        $posts = Profile::where('title', $cond_title)->get();
+    } else {
+        // それ以外はすべてのニュースを取得する
+        $posts = Profile::all();
+    }
+    return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+}
+
 
 
 }
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -10,7 +10,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>Myプロフィール</h2>
+                <h2>プロフィール新規作成</h2>
                 <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/formdata">
 
                 @if (count($errors) > 0)
@@ -40,14 +40,14 @@
                     <div class="form-group row">
                         <label class="col-md-2" for="hobby">趣味</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="hobby" rows="5">{{ old('hobby') }}</textarea>
+                            <textarea class="form-control" name="hobby" rows="20">{{ old('hobby') }}</textarea>
                         </div>
                     </div>
 
                     <div class="form-group row">
                         <label class="col-md-2" for="introduction">自己紹介欄</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="introduction" rows="10">{{ old('introduction') }}</textarea>
+                            <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
                     </div>
 
                     {{ csrf_field() }}
